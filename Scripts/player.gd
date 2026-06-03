849035b697645cfc4f0807bc83cef4dfcad8b01a
eli-sium <elialomar99@gmail.com>
2026-06-03 20:06:25 +0200

    Implemented Lock In mechanic

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,13 +1,63 @@
 extends CharacterBody2D
 
+#Tweakable variables in editor.
+@export var SPEED = 100.0
+@export var JUMP_VELOCITY = -400.0
+@export var heatIncreaseOnJump = 10
+@export var heatIncreaseRateOnMove = 0.1
+@export var heatDecreaseRate = 5
 
-const SPEED = 100.0
-const JUMP_VELOCITY = -400.0
+var currentHeat = 0.0
+var lockedIn = false
+var lastDirectionPressed
+var framesSinceLastDecrease = 0
+
+@onready var animation_tree : AnimationTree = $AnimationTree
+signal heatChange
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-
+func heatIncrease(heatToIncrease):
+	currentHeat += heatToIncrease
+	emit_signal("heatChange", heatToIncrease)
+	if currentHeat >= 100 and !lockedIn:
+		lockedIn = true
+		
+func heatDecrease():
+	framesSinceLastDecrease += 1
+	if framesSinceLastDecrease == 6:
+		currentHeat -= heatDecreaseRate
+		emit_signal("heatChange", (heatDecreaseRate*-1))
+		framesSinceLastDecrease = 0
+		if currentHeat<=0:
+			lockedIn = false;
+		
+func jump():
+	# Jump.
+	velocity.y = JUMP_VELOCITY
+		
+	# Heat management.
+	if !lockedIn:
+		heatIncrease(heatIncreaseOnJump)
+				
+func move():
+	var direction
+	if !lockedIn:
+		direction = Input.get_axis("ui_left", "ui_right")
+		lastDirectionPressed = direction	
+	else:
+		direction = lastDirectionPressed
+		
+	if direction:
+		velocity.x = direction * SPEED
+		if !lockedIn:
+			heatIncrease(heatIncreaseRateOnMove)
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		
+	move_and_slide()
+			
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
@@ -15,14 +65,23 @@ func _physics_process(delta):
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		jump()
 
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	move()
+	
+	if lockedIn:
+		heatDecrease()	
+	
+	# Animation handling
+	"""
+	if direction != Vector2.ZERO:
+		animation_tree["parameters/conditions/is_moving"] = true
+		animation_tree["parameters/conditions/not_moving"] = false
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
-	move_and_slide()
+		animation_tree["parameters/conditions/is_moving"] = false
+		animation_tree["parameters/conditions/not_moving"] = true
+	"""
+		
+		
+	
